8bef26a73b22bc1b6a216aaf3d320957fb908a73
grzeimann <grzeimann@gmail.com>
2019-01-29 09:57:53 -0600

    new feature

--- quick_reduction.py
+++ quick_reduction.py
@@ -267,7 +267,7 @@ def output_fits(image, fn):
                        fplane_file=args.fplane_file)
         wcs = A.setup_TP(args.ra, args.dec, A.rot, crx, 
                          cry, x_scale=-imscale, y_scale=imscale)
-    header = wcs.wcs.to_header()
+    header = wcs.to_header()
     print(header, type(header))
     F = fits.PrimaryHDU(np.array(image, 'float32'))
     for hi in header:
