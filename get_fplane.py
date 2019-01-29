a34b62302dedb09fd45af5bc6e4a20eb301bb133
grzeimann <grzeimann@gmail.com>
2019-01-29 09:17:27 -0600

    first commit of Jan's get_fplane

---
+++ get_fplane.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 16:33:56 2018
+
+@author: gregz
+"""
+
+try:
+    # Python 3
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+except ImportError:
+    # Python 2
+    from urllib2 import urlopen, HTTPError
+
+import datetime
+now = datetime.datetime.now()
+
+def get_fplane(filename, datestr='', actpos=False, full=True):
+
+    url = 'https://luna.mpe.mpg.de/fplane/' + datestr
+
+    if actpos:
+        url += '?actual_pos=1'
+    else:
+        url += '?actual_pos=0'
+
+    if full:
+        url += '&full_fplane=1'
+    else:
+        url += '&full_fplane=0'
+
+    try:
+        resp = urlopen(url)
+    except HTTPError as e:
+        raise Exception(' Failed to retrieve fplane file, server '
+                        'responded with %d %s' % (e.getcode(), e.reason))
+
+    with open(filename, 'w') as f:
+        f.write(resp.read().decode())
+
+date = '%04d%02d%02d' % (now.year, now.month, now.day)
+get_fplane('fplane%s.txt' % date)
