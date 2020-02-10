75e262a55fb941b729f9137b440870648bf79494
grzeimann <grzeimann@gmail.com>
2020-02-10 10:38:52 -0600

    millum update

--- quick_reduction.py
+++ quick_reduction.py
@@ -1693,8 +1693,8 @@ def fit_astrometry(f, A1, thresh=10.):
     return A1
 
 
-def cofes_plots(ifunums, specnums, filename_array, outfile_name, fF, vmin=-2.0,
-                vmax=20.):
+def cofes_plots(ifunums, specnums, filename_array, outfile_name, fF, vmin=-1.,
+                vmax=10.):
     """
     filename_array is an array-like object that contains the filenames
     of fits files to plot. The output plot will be the shape of the input array.
