30308e7363e6a2c5e131f18e0e64d168fb0be18a
grzeimann <grzeimann@gmail.com>
2020-02-11 14:07:36 -0600

    debugging

--- build_remedy_call_for_date.py
+++ build_remedy_call_for_date.py
@@ -56,6 +56,7 @@ for tarfolder in tarfolders:
             flag = False
 with open('%s_calls' % date, 'w') as out_file:       
     for chunk in np.array_split(tarlist, ncalls):
+        print(chunk[0][0])
         calls = [call % (date, ch[0]) for ch in chunk]
         call_str = '; '.join(calls)
         out_file.write(call_str + '\n')
