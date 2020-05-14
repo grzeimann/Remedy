91b4351e546b6f7a690e7e7bd1ea6e3a1a79201c
grzeimann <grzeimann@gmail.com>
2020-05-13 19:11:22 -0500

    update

---
+++ show_h5info.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 19:09:28 2020
+
+@author: gregz
+"""
+
+import tables
+import sys
+
+filename = sys.argv[1]
+
+t = tables.open_file(filename)
+
+print(t.root.Survey)
