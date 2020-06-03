c1d345d637c85fb64432e101b872fd710313ba13
grzeimann <grzeimann@gmail.com>
2020-06-03 14:22:29 -0500

    first commit

--- investigate_amp_cal.py
+++ investigate_amp_cal.py
@@ -89,5 +89,14 @@ for i, master in enumerate([masterflt, mastertwi, mastersci]):
                  fill_value='extrapolate')
     modelimageF = J(bigF)
     flatimage = master / modelimage / modelimageF
+    if i == 0:
+        Flat = flatimage * 1.
+    if i == 2:
+        skyimage = Flat * modelimage
+        skysub = master - skyimage
+        fits.HDUList([fits.PrimaryHDU(np.array(master, dtype='float32')),
+                      fits.ImageHDU(np.array(skyimage, dtype='float32')),
+                      fits.ImageHDU(np.array(skysub, dtype='float32'))]).writeto('masterskysub_%03d%s.fits' %
+                      (ifuslot, amp), overwrite=True)
     fits.PrimaryHDU(np.array(flatimage, dtype='float32')).writeto('%s_%03d%s.fits' %
                    (names[i], ifuslot, amp), overwrite=True)
