ae989c3ea2ba8044bf5fd4d7563c113453d26c79
grzeimann <grzeimann@gmail.com>
2020-06-12 10:27:45 -0500

    update

--- quick_reduction.py
+++ quick_reduction.py
@@ -1038,7 +1038,7 @@ def reduce_ifuslot(ifuloop, h5table, tableh5):
             trace = 0. * trace
         readnoise = h5table[ind]['readnoise']
         masterflt = h5table[ind]['masterflt']
-        mastersci = h5table[ind]['mastertwi']
+        mastersci = h5table[ind]['masterscii']
         maskspec = h5table[ind]['maskspec']
         masterbias = h5table[ind]['masterbias']
         try:
