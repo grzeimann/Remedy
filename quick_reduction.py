2d99bea9f558d7b5b1f9c235dfbed14a1416bba4
grzeimann <grzeimann@gmail.com>
2020-07-16 19:34:28 -0500

    update

--- quick_reduction.py
+++ quick_reduction.py
@@ -2325,8 +2325,10 @@ sel1 = []
 for z in uzips:
     if z not in ZIPs:
         continue
+    print(np.where(z == zips)[0])
     sel1.append(np.where(z == zips)[0])
     allzips.append(z)
+
 ziploop = np.array(np.hstack(sel1), dtype=int)
 print(ziploop)
 nslots = len(ziploop) / 4
