4e6d24761eb5988cc0bd3256ef6c1382b592bd9a
grzeimann <grzeimann@gmail.com>
2020-07-17 16:08:01 -0500

    update

--- make_mosaic_cube.py
+++ make_mosaic_cube.py
@@ -337,7 +337,7 @@ for jk, h5file in enumerate(h5files):
         limage = (cimage-v)/norm_array[jk]
         bl, bottom_var = biweight(limage[(binimage<0.01) * np.isfinite(cimage)], calc_std=True)
 
-        threshold = np.sqrt((nimage * 0.15)**2 + bottom_var**2)
+        threshold = np.sqrt((nimage * 0.25)**2 + (3.*bottom_var)**2)
         flagged = np.abs(diff_image[yc[gsel], xc[gsel]]) > threshold[yc[gsel], xc[gsel]]
         spectra[np.where(gsel)[0][flagged]] = np.nan
         args.log.info('%i fibers flagged for too large of a difference' % flagged.sum())
