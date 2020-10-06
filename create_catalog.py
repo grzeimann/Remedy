78f57f98d72fb098999da4ed8c3d0e772afd341b
grzeimann <grzeimann@gmail.com>
2020-10-06 12:57:18 -0500

    first commit

--- create_catalog.py
+++ create_catalog.py
@@ -138,7 +138,7 @@ for niter in np.arange(2):
 process = psutil.Process(os.getpid())
 log.info('Memory Used: %0.2f GB' % (process.memory_info()[0] / 1e9))
 T = Table([RA, DEC, NAME, GMAG, SN], names=['RA', 'Dec', 'shotid', 'gmag', 'sn'])
-fits.HDUList([fits.PrimaryHDU(), fits.BinHDUTable(T), fits.ImageHDU(SPEC),
+fits.HDUList([fits.PrimaryHDU(), fits.BinTableHDU(T), fits.ImageHDU(SPEC),
               fits.ImageHDU(ERROR), fits.ImageHDU(WEIGHT)]).writeto(outname, overwrite=True)
 
 log.info('Total Number of sources is %i for %i shots' % (totN, Nshots))
