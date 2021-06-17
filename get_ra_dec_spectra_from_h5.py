f2eab0665240efa90ff55c711887ea3eede0ccce
grzeimann <grzeimann@gmail.com>
2021-06-17 12:26:48 -0500

    update

--- get_ra_dec_spectra_from_h5.py
+++ get_ra_dec_spectra_from_h5.py
@@ -12,19 +12,49 @@ import sys
 from astropy.io import fits
 from astropy.table import Table
 import os.path as op
+import numpy as np
+import subprocess
 
 filename = sys.argv[1]
 
+base = '/work2/00115/gebhardt/maverick/gettar'
+
+def get_structaz(filename):
+    date = filename.split('_')[0]
+    obs = filename.split('_')[1]
+    fn = op.join(base, '%ssci' % date[:6])
+    process = subprocess.Popen('cat %s | grep %s | grep %s ' %
+                                       (fn, date, obs),
+                                       stdout=subprocess.PIPE, shell=True)
+    line = process.stdout.readline()
+    b = line.rstrip().decode("utf-8")
+    b = b.split(' ')[7]
+    return b
+
+structaz = get_structaz(filename)
 t = tables.open_file(filename)
 ra = getattr(t.root, 'Info').cols.ra[:]
 dec = getattr(t.root, 'Info').cols.dec[:]
+fwhm =getattr(t.root, 'Survey').cols.fwhm[0]
+specid = getattr(t.root, 'Info').cols.specid[:]
+ifuid = getattr(t.root, 'Info').cols.ifuid[:]
+ifuslot = getattr(t.root, 'Info').cols.ifuslot[:]
+amp = getattr(t.root, 'Info').cols.amp[:]
+N = np.arange(len(amp))
+strings = []
+for s, ifid, ifsl, ap, n in zip(specid, ifuid, ifuslot, amp, N):
+    st = '%03d_%03d_%03d_%s_%03d' % (s, ifsl, ifid, ap, int(n % 112))
+    strings.append(st)
 
 spectra = getattr(getattr(t.root, 'Fibers').cols, 'spectrum')[:]
 error = getattr(getattr(t.root, 'Fibers').cols, 'error')[:]
 
-T = Table([ra, dec], names=['ra', 'dec'])
-L = fits.HDUList([fits.PrimaryHDU(), fits.BinTableHDU(T), fits.ImageHDU(spectra),
-                  fits.ImageHDU(error)])
-fitsname = op.basename(filename).split('.h5')[0] + '.fits'
-L.writeto(fitsname, overwrite=True)
-t.close()
+spectra[np.isnan(spectra)] = 0.
+error[np.isnan(error)] = 0.
+
+T = Table([ra, dec, strings], names=['ra', 'dec', 'multiname'])
+h = fits.PrimaryHDU()
+h.header['fwhm'] = fwhm
+h.header['structaz'] = structaz
+L = fits.HDUList([h, fits.ImageHDU(spectra),
+                  fits.ImageHDU(error), fits.BinTableHDU(T)])
