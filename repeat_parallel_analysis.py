c49fa4ef3348491ae57fcbb5e17d1dcf0fa6061d
grzeimann <grzeimann@gmail.com>
2022-05-12 06:34:53 -0500

    first

--- repeat_parallel_analysis.py
+++ repeat_parallel_analysis.py
@@ -32,6 +32,7 @@ keep2 = []
 cnt = 0
 for i in inds:
     idx, d2d, d3d = s[i].match_to_catalog_sky(S)
+    print(len(idx))
     sel = np.where((d2d.arcsec < 1.)  * (f[1].data['shotid'][i] != shotids))[0]
     if (cnt % 100) == 0:
         print('We are at %i' % (cnt+1))
