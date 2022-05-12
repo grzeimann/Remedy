4562cc35c72d34adee9d521212a388bc763065b9
grzeimann <grzeimann@gmail.com>
2022-05-12 06:30:11 -0500

    first

---
+++ repeat_parallel_analysis.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 06:12:29 2022
+
+@author: gregz
+"""
+
+from astropy.io import fits
+import numpy as np
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
+filename1 = '/work/03730/gregz/maverick/parallel/HETVIPS_continuum_v1.0_unique.fits'
+filename2 = '/work/03730/gregz/maverick/parallel/HETVIPS_continuum_v1.0.fits'
+
+f = fits.open(filename1)
+g = fits.open(filename2)
+
+inds = np.where(f[1].data['sn'] > 100.)[0]
+inds2 = np.where(g[1].data['sn'] > 20.)[0]
+
+s = SkyCoord(f[1].data['RA'][inds2]*u.deg, f[1].data['Dec'][inds2]*u.deg)
+S = SkyCoord(g[1].data['RA'][inds2]*u.deg, g[1].data['Dec'][inds2]*u.deg)
+shotids = g[1].data['shotid'][inds2]
+spectra = f[2].data[inds]
+spectra2 = g[2].data[inds2]
+
+keep1 = []
+keep2 = []
+cnt = 0
+for i in inds:
+    idx, d2d, d3d = s[i].match_to_catalog_sky(S)
+    sel = np.where((d2d.arcsec < 1.)  * (f[1].data['shotid'][i] != shotids))[0]
+    if len(sel) > 0.:
+        cnt += len(sel)
+        keep1.append(i)
+        keep2.append(sel)
+        
+diff = np.zeros((cnt, 1036))
+cnt = 0
+for i, sel in zip(keep1, keep2):
+    for s in sel:
+        diff[cnt] = spectra[i] - spectra2[s]
+        cnt += 1
+fits.PrimaryHDU(diff).writeto('/work/03730/gregz/maverick/parallel/repeat_difference.fits')
