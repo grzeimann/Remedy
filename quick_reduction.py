2c728d9b1a6d0d0856d66c19950f6798a04cf57d
grzeimann <grzeimann@gmail.com>
2023-11-13 13:57:58 -0600

    Update quick_reduction.py

--- quick_reduction.py
+++ quick_reduction.py
@@ -2629,23 +2629,23 @@ if op.exists(pname):
                          np.array(Pan['yApMag']))
 else:
     try:
-         
-        Pan = Catalogs.query_region("%s %s" % (ra, dec), radius=11./60., 
-                                       catalog="Panstarrs")
-        raC, decC, gC, rC, iC, zC, yC = (np.array(Pan['raMean']), np.array(Pan['decMean']),
-                         np.array(Pan['gMeanApMag']), np.array(Pan['rMeanApMag']),
-                         np.array(Pan['iMeanApMag']), np.array(Pan['zMeanApMag']),
-                         np.array(Pan['yMeanApMag']))
-    except:
-        log.info('Panstarrs initial query failed, '
-                 'trying with small coord adjustment')
         Pan = Catalogs.query_region("%s %s" % (ra, dec), radius=11./60., 
                                        catalog="Panstarrs", data_release="dr2",
                                        table="stack")
         raC, decC, gC, rC, iC, zC, yC = (np.array(Pan['raMean']), np.array(Pan['decMean']),
                          np.array(Pan['gApMag']), np.array(Pan['rApMag']),
                          np.array(Pan['iApMag']), np.array(Pan['zApMag']),
-                         np.array(Pan['yApMag']))
+                         np.array(Pan['yApMag'])) 
+        
+    except:
+        log.info('Panstarrs initial query failed, '
+                 'trying with small coord adjustment')
+        Pan = Catalogs.query_region("%s %s" % (ra, dec), radius=11./60., 
+                                       catalog="Panstarrs")
+        raC, decC, gC, rC, iC, zC, yC = (np.array(Pan['raMean']), np.array(Pan['decMean']),
+                         np.array(Pan['gMeanApMag']), np.array(Pan['rMeanApMag']),
+                         np.array(Pan['iMeanApMag']), np.array(Pan['zMeanApMag']),
+                         np.array(Pan['yMeanApMag']))
     Pan.write(pname, format='ascii.fixed_width_two_line')
 
 
