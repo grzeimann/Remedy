8a466ec8193ca33da55edf77e6dd9af004e69a24
grzeimann <grzeimann@gmail.com>
2025-05-05 11:33:03 -0500

    first commit

--- vdfi_combine_detections.py
+++ vdfi_combine_detections.py
@@ -12,6 +12,8 @@ import glob
 import os.path as op
 from input_utils import setup_logging
 import warnings
+from astropy.table import Table
+from astropy.table import vstack
 
 # Turn off annoying warnings (even though some deserve attention)
 warnings.filterwarnings("ignore")
@@ -35,16 +37,24 @@ filenames = glob.glob('/work/03730/gregz/ls6/PHATTER-VIRUS/detections/detect*.fi
 N = cfht[0].data.shape[0]
 vdfi_cube = np.zeros((N, N, 1036), dtype=np.float32)
 vdfi_error = np.zeros((N, N, 1036), dtype=np.float32)
+vdfi_sn = np.zeros((N, N, 3001), dtype=np.float32)
+tables = []
 for filename in filenames:
     n = op.basename(filename)
-    xi, xj = (int(n.split('_')[1]), int(n.split('_')[2]))
+    xi, xj = (int(n.split('_')[2]), int(n.split('_')[3]))
     with fits.open(filename, memmap=True) as hdulist:
         log.info('Loaded %02d, %02d' % (xi, xj))
         # Extract RA/Dec center from input grids
         x = int(xgrid[xi, xj] - 29.5)
         y = int(ygrid[xi, xj] - 29.5)
-        vdfi_cube[y:y+60,x:x+60] = hdulist[0].data[1:-1,1:-1] * 1.
-        vdfi_error[y:y+60,x:x+60] = hdulist[1].data[1:-1,1:-1] * 1.
+        vdfi_cube[y:y+60,x:x+60] = hdulist[4].data[1:-1,1:-1] * 1.
+        vdfi_error[y:y+60,x:x+60] = hdulist[5].data[1:-1,1:-1] * 1.
+        vdfi_sn[y:y+60,x:x+60] = hdulist[6].data[1:-1,1:-1] * 1.
+        tables.append(Table(hdulist[3].data))
+
+combined_table = vstack(tables)
+combined_table.write('all_detections.txt', format='ascii.fixed_width_two_line',
+                     overwrite=True)
 
 header = wcs.to_header()
 
@@ -80,4 +90,19 @@ header['CUNIT2'] = 'deg'
 header['CUNIT3'] = 'Angstrom'
 header['SPECSYS'] = 'TOPOCENT'
 F = fits.PrimaryHDU(vdfi_error, header=header)
+F.writeto(name, overwrite=True)
+name = op.basename('%s_sncube.fits' % surname)
+header['CRPIX1'] = (N+1) / 2.
+header['CRPIX2'] = (N+1) / 2.
+header['CDELT3'] = 0.64
+header['CRPIX3'] = 1.
+header['CRVAL3'] = 3560.
+header['CTYPE1'] = 'RA---TAN'
+header['CTYPE2'] = 'DEC--TAN'
+header['CTYPE3'] = 'WAVE'
+header['CUNIT1'] = 'deg'
+header['CUNIT2'] = 'deg'
+header['CUNIT3'] = 'Angstrom'
+header['SPECSYS'] = 'TOPOCENT'
+F = fits.PrimaryHDU(vdfi_sn, header=header)
 F.writeto(name, overwrite=True)
