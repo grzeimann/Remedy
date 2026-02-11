87cfdf87a4e74a662b3c548892a53f699dc287b7
grzeimann <grzeimann@gmail.com>
2026-02-11 14:23:38 -0600

    percentile change

--- make_mosaic_cube.py
+++ make_mosaic_cube.py
@@ -579,6 +579,7 @@ def evaluate_cube_stats(cube, ecube, weightcube, surname, log,
         if cont_inliers.size >= 10:
             p005 = float(np.nanpercentile(cont_inliers, 2.5))
             p995 = float(np.nanpercentile(cont_inliers, 97.5))
+            print(p005, p995)
             if (not np.isfinite(p005)) or (not np.isfinite(p995)) or (p005 == p995):
                 p005, p995 = float(np.nanmin(cont_inliers)), float(np.nanmax(cont_inliers))
         else:
